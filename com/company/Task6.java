0648a7c77601db20adb66b4c321027566d8793b9
archiechop1 <alenka.dzyum1@gmail.com>
2022-12-19 20:08:49 +0200

    test

--- com/company/Task6.java
+++ com/company/Task6.java
@@ -4,17 +4,67 @@ import java.util.Scanner;
 
 public class Task6 {
     public static void main(String[] args) {
-        currency();
-    }
+        boolean isTrue=true;
 
-    public static float currency(){
-        float euro, dollar;
-        System.out.println("Please enter an amount of money in euro: ");
-        Scanner scn = new Scanner(System.in);
-        euro = scn.nextFloat();
-        dollar = euro * 1.06f; //the exchange rate as of 19th December 2022
-        float result = dollar;
-        System.out.println("The amount of money in dollars is " + result + " $.");
-        return result;
+        while (isTrue) {
+            System.out.println("Please enter the number of month!");
+            Scanner scn = new Scanner(System.in);
+            int month = scn.nextInt();
+
+
+            switch (month) {
+                case 1:
+                    System.out.println("January");
+                    isTrue=false;
+                    break;
+                case 2:
+                    System.out.println("February");
+                    isTrue=false;
+                    break;
+                case 3:
+                    System.out.println("March");
+                    isTrue=false;
+                    break;
+                case 4:
+                    System.out.println("April");
+                    isTrue=false;
+                    break;
+                case 5:
+                    System.out.println("May");
+                    isTrue=false;
+                    break;
+                case 6:
+                    System.out.println("June");
+                    isTrue=false;
+                    break;
+                case 7:
+                    System.out.println("July");
+                    isTrue=false;
+                    break;
+                case 8:
+                    System.out.println("August");
+                    isTrue=false;
+                    break;
+                case 9:
+                    System.out.println("September");
+                    isTrue=false;
+                    break;
+                case 10:
+                    System.out.println("October");
+                    isTrue=false;
+                    break;
+                case 11:
+                    System.out.println("November");
+                    isTrue=false;
+                    break;
+                case 12:
+                    System.out.println("December");
+                    isTrue=false;
+                    break;
+                default:
+                    System.out.println("This month doesn't exist!");
+            }
+
+        }
     }
 }
